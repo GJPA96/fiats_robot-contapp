f418708c91c756e8e76bb4e55a84576996c52d57
GJPA96 <yaquinito@gmail.com>
2026-06-08 22:50:29 -0400

    cambio en la verificacion de usuario

--- Contapp.py
+++ Contapp.py
@@ -30,7 +30,7 @@ def verificar_password():
         
         if boton_enviar:
             # Compara directamente con el archivo secrets.toml
-            if password_ingresada == st.secrets["PASSWORD_GENERAL"]:
+            if password_ingresada == st.secrets["auth"]["password"]:
                 st.session_state["autenticado"] = True
                 st.rerun()  # Forza a Streamlit a recargar el código ya autenticado
             else:
